42707a600c19dfb20ad8bc75184e2b4ba9af4143
oooolga <olga.xu@mail.utoronto.ca>
2018-03-10 05:55:14 -0500

    commit changes

--- part2/train_valid.py
+++ part2/train_valid.py
@@ -17,19 +17,68 @@ def _train(model, train_loader, optimizer):
 		optimizer.zero_grad()
 		output = model(data)
 
+		target = target.view(-1, 1)
 		loss = F.binary_cross_entropy(F.sigmoid(output), target.float())
 		loss.backward()
 		optimizer.step()
 
-		print '| | iter: {}\tloss: {}'.format(batch_idx, loss.data[0])
+		if batch_idx % 50 == 0:
+			print '| | iter: {}\tloss: {:.4f}'.format(batch_idx, loss.data[0])
 
 	return loss
+
+def _evaluate_data_set(model, data_loader):
+
+	model.eval()
+	total_loss, correct = 0, 0
+	total_data, total_batch = 0, 0
+
+	for batch_idx, (data, target) in enumerate(data_loader):
+
+		if use_cuda:
+			data, target = data.cuda(), target.cuda()
+
+		data, target = Variable(data, volatile=True, requires_grad=False), \
+					Variable(target, requires_grad=False)
+
+		output = model(data)
+
+		out_eval = F.sigmoid(output)
+
+		target = target.view(-1, 1)
+		total_loss += F.binary_cross_entropy(out_eval, target.float()).data[0]
+
+		out_eval = out_eval.data-0.5
+
+		predicted = torch.ceil(out_eval).int()
+		target = target.int()
+
+		correct += (predicted == target.data).sum()
+
+		total_data += len(data)
+		total_batch += 1
+
+	avg_loss = total_loss / float(total_batch)
+	accuracy = correct / float(total_data)
+	return avg_loss, accuracy
 
 
 def run(model, train_loader, valid_loader, test_loader, total_epoch, lr, momentum):
 	optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
 
+	print('| epoch: {}'.format(0))
+	avg_loss, accuracy = _evaluate_data_set(model, train_loader)
+	print('| train loss: {:.4f}\ttrain acc: {:.4f}'.format(avg_loss, accuracy))
+	avg_loss, accuracy = _evaluate_data_set(model, valid_loader)
+	print('| valid loss: {:.4f}\tvalid acc: {:.4f}'.format(avg_loss, accuracy))
+
 	for epoch in range(1, total_epoch+1):
 
 		print('| epoch: {}'.format(epoch))
-		loss = _train(model, train_loader, optimizer)
+		_ = _train(model, train_loader, optimizer)
+		avg_loss, accuracy = _evaluate_data_set(model, train_loader)
+		print('| train loss: {:.4f}\ttrain acc: {:.4f}'.format(avg_loss, accuracy))
+		avg_loss, accuracy = _evaluate_data_set(model, valid_loader)
+		print('| valid loss: {:.4f}\tvalid acc: {:.4f}'.format(avg_loss, accuracy))
+		avg_loss, accuracy = _evaluate_data_set(model, test_loader)
+		print('| test loss: {:.4f}\ttest acc: {:.4f}'.format(avg_loss, accuracy))
